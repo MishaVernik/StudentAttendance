f21bf1e78354c86620cefbdbac3de33b8cc5868e
MishaVernik <nodus1.5.3@gmail.com>
2019-09-10 12:02:39 +0300

    allowed hosts .herokuapp.com

--- StudentAttendance/settings.py
+++ StudentAttendance/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c3mu%_6ild$t+iz_9mfu#=xm^_gj(s%qt5*iek2u1e95mu0=xs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', '.herokuapp.com', 'localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
