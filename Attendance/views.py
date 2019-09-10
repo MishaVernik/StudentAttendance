274b32c670effc65779cff7c617a4985b575a285
MishaVernik <nodus1.5.3@gmail.com>
2019-09-10 17:57:37 +0300

    UI changes

--- Attendance/views.py
+++ Attendance/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
+@login_required
 def home(request):
     return render(request, 'home.html')
 
