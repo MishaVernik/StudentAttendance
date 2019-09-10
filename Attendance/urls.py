261aa08a5cfe1ccb2e37eec8b626089f59afad6e
MishaVernik <nodus1.5.3@gmail.com>
2019-09-10 16:59:32 +0300

    UI changes

--- Attendance/urls.py
+++ Attendance/urls.py
@@ -1,7 +1,6 @@
-
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
-
+import imaplib
 from . import views
 
 
@@ -9,7 +8,7 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     #url(r'^login/$', django.contrib.auth.views.LoginView, {'template_name': 'login.html'}, name='login'),
-    #url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(next_page="login"), name='logout'),
     url(r'^signup/$', views.signup, name='signup'),
    # url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
     #    views.activate, name='activate'),
