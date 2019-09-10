4a090d2d190836f7b528f9bd8ae6754c552706ef
MishaVernik <nodus1.5.3@gmail.com>
2019-09-10 16:27:37 +0300

    UI changes

--- Attendance/views.py
+++ Attendance/views.py
@@ -14,6 +14,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
+            # TODO: add user into PostgreSQL
             return redirect('home')
     else:
         form = UserCreationForm()
