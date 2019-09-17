46f080c39186ce6a364b658319bf4ac179907df6
MishaVernik <nodus1.5.3@gmail.com>
2019-09-17 19:17:55 +0300

    registration menu

--- Attendance/urls.py
+++ Attendance/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     url(r'^accounts/profile/$', views.home, name="home"),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     #url(r'^login/$', django.contrib.auth.views.LoginView, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(next_page="login"), name='logout'),
     url(r'^signup/$', views.signup, name='signup'),
