e4a7615e7d312db55a81390f815e71b468e1b023
MishaVernik <nodus1.5.3@gmail.com>
2019-09-18 00:26:27 +0300

    deletd files

--- Attendance/views.py
+++ Attendance/views.py
@@ -3,8 +3,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
-import psycopg2
-from psycopg2 import Error
 
 from Attendance.forms import SignUpForm
 
@@ -13,33 +11,6 @@ from Attendance.forms import SignUpForm
 def home(request):
     return render(request, 'home.html')
 
-def add_student(first_name, second_name, group, email, github, username, password):
-    try:
-        connection = psycopg2.connect(user="rsmoejupwkoewe",
-                                      password="b884a8e0f70ca48fffe7f68007042862a0a19ed25223611f5db50005c14f82cf",
-                                      host="ec2-54-221-212-126.compute-1.amazonaws.com",
-                                      port="5432",
-                                      database="dbqloi6h7obslf")
-
-        cursor = connection.cursor()
-
-        create_table_query = ''' INSERT INTO public.students(
-                            first_name, second_name, email, "group", github, password, username)
-                            VALUES (%s, %s, %s, %s, %s, %s, %s);
-                              '''
-        recordTuple = (first_name, second_name, email, group, github, password, username)
-        cursor.execute(create_table_query, recordTuple)
-        connection.commit()
-        print("Records successfully inserted in PostgreSQL")
-
-    except (Exception, psycopg2.DatabaseError) as error:
-        print("Error while doing smth in PostgreSQL", error)
-    finally:
-        # closing database connection.
-        if (connection):
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
 
 def signup(request):
 
