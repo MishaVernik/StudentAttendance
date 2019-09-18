56a37be3fe0e7684e6bc411ea5e9538a0fb6e7c0
MishaVernik <nodus1.5.3@gmail.com>
2019-09-18 12:58:11 +0300

    weee

--- StudentAttendance/settings.py
+++ StudentAttendance/settings.py
@@ -16,9 +16,6 @@ import django_heroku
 django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
