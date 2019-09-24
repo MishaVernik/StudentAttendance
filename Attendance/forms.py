98c0d79638c0c1f668a7c4bc7cac354e349785e7
MishaVernik <nodus1.5.3@gmail.com>
2019-09-24 15:33:53 +0300

    new forms sudent/teacher

--- Attendance/forms.py
+++ Attendance/forms.py
@@ -20,8 +20,8 @@ class SignUpTeacherForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True, help_text='First name')
     last_name = forms.CharField(max_length=30, required=True, help_text='Last name')
     email = forms.EmailField(max_length=254, required=True, help_text='Required. Inform a valid email address.')
-    group = forms.CharField(max_length=10, required=True, help_text='Your group (ex: КВ-71)')
-    github = forms.CharField(max_length=254, required=False, help_text='Optional')
+    groups = forms.CharField(max_length=254, required=True, help_text='Groups (ex: КВ-71, KB-72, ..)')
+    faculty = forms.CharField(max_length=254, required=False, help_text='Optional')
     class Meta:
         model = User
-        fields = ['username','first_name', 'last_name','email','group','github', 'password1', 'password2']
+        fields = ['username','first_name', 'last_name','email','groups','faculty', 'password1', 'password2']
