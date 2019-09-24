97a7512783b370815504e767bb8fce77ce252ee9
MishaVernik <nodus1.5.3@gmail.com>
2019-09-24 15:42:29 +0300

    new forms sudent/teacher

--- StudentAttendance/urls.py
+++ StudentAttendance/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from Attendance.views import signup
+from Attendance.views import signupStudent, signUpTeacher
 from django.contrib import admin
 from django.urls import path, include
 
