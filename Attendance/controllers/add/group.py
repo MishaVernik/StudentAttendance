8ed492f450b52e8369bd01f10ff0c82f5b1b53ab
Misha Vernik <nodus1.5.3@gmail.com>
2019-12-10 19:38:40 +0200

    insert into groupd + some funtionality for subjects

--- Attendance/controllers/add/group.py
+++ Attendance/controllers/add/group.py
@@ -1,4 +1,9 @@
+import psycopg2
 from django.shortcuts import render
+
+from Attendance.controllers.add.subject import insert_into_subjects
+from Attendance.controllers.get.id import get_teachers_id
+from Attendance.controllers.get.sql_connection import get_sql_connection
 
 
 def index(request):
@@ -11,14 +16,61 @@ def index(request):
         'add_group.html')
 
 
+def get_id(teacher_id, group, semester):
+    try:
+        connection = get_sql_connection()
+        cursor = connection.cursor()
+        postgre_sql_select_query = 'SELECT id from public.schedule WHERE teacher_id=%s AND "group"=%s AND semester=%s;'
+        cursor.execute(postgre_sql_select_query, (teacher_id, group, semester), )
+        mobile_records = cursor.fetchall()
+
+        for row in mobile_records:
+            return row[0]
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error ifStudentOnTheLecture while doing smth in PostgreSQL", error)
+    finally:
+        # closing database connection.
+        cursor.close()
+        connection.close()
+        print("PostgreSQL ifStudentOnTheLecture connection is closed")
+    return -1
+
+
 def add_group(request):
     """
-    Функция отображения для домашней страницы сайта.
+
     """
     print(request.user)
-    print(request.POST.get('semester'))
-    print(request.POST.get('subject'))
-    print(request.POST.get('group'))
+    semester = request.POST.get('semester').split()
+    subject = request.POST.get('subject')
+    teacher_id = get_teachers_id(str(request.user))
+
+    groups = request.POST.get('group').split(',')
+    for group in groups:
+        try:
+            connection = get_sql_connection()
+            cursor = connection.cursor()
+            create_table_query = ''' INSERT INTO public.schedule(
+                                teacher_id, "group", semester)
+                                VALUES (%s, %s, %s);
+                                  '''
+
+            record_tuple = (teacher_id, group, semester[1])
+            cursor.execute(create_table_query, record_tuple)
+            connection.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Error while doing smth in PostgreSQL", error)
+        finally:
+            # closing database connection.
+            cursor.close()
+            connection.close()
+
+        if len(subject) > 0:
+            schedule_id = get_id(teacher_id=teacher_id, group=group, semester=semester[1])
+            if schedule_id == -1:
+                pass
+            else:
+                insert_into_subjects(schedule_id, subject)
     return render(
         request,
-        'add_group.html')
+        'home.html')
--- Attendance/controllers/add/subject.py
+++ Attendance/controllers/add/subject.py
@@ -1,4 +1,7 @@
+import psycopg2
 from django.shortcuts import render
+
+from Attendance.controllers.get.sql_connection import get_sql_connection
 
 
 def index(request):
@@ -8,6 +11,26 @@ def index(request):
     return render(
         request,
         'add_group.html')
+
+
+def insert_into_subjects(schedule_id, subject):
+    try:
+        connection = get_sql_connection()
+        cursor = connection.cursor()
+        create_table_query = ''' INSERT INTO public.subjects(
+                            schedule_id, subject)
+                            VALUES (%s, %s);
+                              '''
+
+        record_tuple = (schedule_id, subject)
+        cursor.execute(create_table_query, record_tuple)
+        connection.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error while doing smth in PostgreSQL", error)
+    finally:
+        # closing database connection.
+        cursor.close()
+        connection.close()
 
 
 def add_subject(request):
